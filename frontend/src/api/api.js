a37773f5b2dc985777bdcbf44536cb8fe481582e
MidhunMohan210 <midhunmohan3210@gmail.com>
2024-02-20 12:37:36 +0530

    api changed

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL:'http://localhost:7000'
-    // baseURL:'https://erp.camet.in/'
+    // baseURL:'http://localhost:7000'
+    baseURL:"https://www.sales.camet.in/"
 
     // baseURL:'https://camet.midhunmohan.online/'
 
