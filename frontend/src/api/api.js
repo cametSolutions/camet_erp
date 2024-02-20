4972d365f0d1a36109d58d6cfd3f1ba891e2d84d
root <root@ubuntu-cloudpanel.localhost>
2024-02-20 09:11:03 +0000

    hostinger server change

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 export default axios.create({
+
     // baseURL:'http://localhost:7000'
     baseURL:"https://www.sales.camet.in/"
-
     // baseURL:'https://camet.midhunmohan.online/'
 
     //hello
