c9782f6a30654396a3b30f8092f2137a57fc40be
MidhunMohan210 <midhunmohan3210@gmail.com>
2024-03-15 15:11:15 +0530

    optimisation added to main

---
+++ frontend/src/pages/primaryUsers/InvoiceList.jsx
@@ -0,0 +1,274 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/no-unknown-property */
+import { useEffect, useState } from "react";
+import api from "../../api/api";
+import { toast } from "react-toastify";
+import Sidebar from "../../components/homePage/Sidebar";
+import { useSelector } from "react-redux";
+import { MdDelete } from "react-icons/md";
+import { FaEdit } from "react-icons/fa";
+import Swal from "sweetalert2";
+import { Link } from "react-router-dom";
+import { HashLoader } from "react-spinners";
+import { IoIosSearch } from "react-icons/io";
+import { FixedSizeList as List } from "react-window";
+import { IoIosArrowRoundBack } from "react-icons/io";
+
+function InvoiceList() {
+  const [parties, setParties] = useState([]);
+  const [invoice, setInvoice] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [refresh, setRefresh] = useState(false);
+  const [loader, setLoader] = useState(false);
+  const [filteredInvoice, setFilteredInvoice] = useState([]);
+
+  const cpm_id = useSelector(
+    (state) => state.setSelectedOrganization.selectedOrg._id
+  );
+
+  useEffect(() => {
+    setLoader(true);
+
+    const fetchInvoice = async () => {
+      try {
+        const res = await api.get(`/api/pUsers/invoiceList/${cpm_id}`, {
+          withCredentials: true,
+        });
+
+        setTimeout(() => {
+          setInvoice(res.data.invoiceList);
+        }, 1000);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoader(false);
+      }
+    };
+    fetchInvoice();
+  }, [cpm_id, refresh]);
+  useEffect(() => {
+    if (search === "") {
+      setFilteredInvoice(invoice);
+    } else {
+      const filtered = invoice.filter((el) =>
+        el?.party?.partyName.toLowerCase().includes(search.toLowerCase())
+      );
+      setFilteredInvoice(filtered);
+    }
+  }, [search, invoice, refresh]);
+
+  const deleteHandler = async (id) => {
+    // Show confirmation dialog
+    const confirmation = await Swal.fire({
+      title: "Are you sure?",
+      text: "This action cannot be undone!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#d33",
+      cancelButtonColor: "#3085d6",
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "Cancel",
+    });
+
+    // If user confirms deletion
+    if (confirmation.isConfirmed) {
+      try {
+        const res = await api.delete(`/api/pUsers/deleteParty/${id}`, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        });
+
+        // Display success message
+        await Swal.fire({
+          title: "Deleted!",
+          text: res.data.message,
+          icon: "success",
+          timer: 2000, // Auto close after 2 seconds
+          timerProgressBar: true,
+          showConfirmButton: false,
+        });
+
+        // Refresh the page
+        setRefresh(!refresh);
+      } catch (error) {
+        toast.error(error.response.data.message);
+        console.log(error);
+      }
+    }
+  };
+
+  console.log(invoice);
+
+  const Row = ({ index, style }) => {
+    const el = filteredInvoice[index];
+    return (
+      <>
+        <div
+          key={index}
+          style={style}
+          className="bg-white p-4 pb-6 drop-shadow-lg mt-4 flex flex-col mx-2 rounded-sm cursor-pointer hover:bg-slate-100  pr-7 "
+        >
+          <div className="flex justify-between w-full gap-3 ">
+            <div className="">
+              <p className="font-bold text-sm">{el?.party?.partyName}</p>
+              {el?.party?.mobileNumber && (
+                <div className="flex">
+                  {/* <p className="font-medium mt-2 text-gray-500 text-sm text-nowrap">
+                    Acc group :
+                  </p> */}
+                  <p className="font-medium mt-2 text-gray-500 text-sm">
+                    {el?.party?.mobileNumber}
+                  </p>
+                </div>
+              )}
+            </div>
+            <div className=" flex flex-col justify-center gap-2">
+              <div className="flex gap-2 text-nowrap">
+                <p className="font-bold">Amount :</p>
+                <p className="font-semibold text-gray-500">
+                  {" "}
+                  {`₹ ${el?.finalAmount}`}
+                </p>
+              </div>
+              {/* <div className="flex gap-2 ">
+                <p className="font-bold">Email :</p>
+                <p className="font-bold text-green-500"> {`${el?.emailID} %`}</p>
+              </div> */}
+            </div>
+          </div>
+          <div className="flex gap-3 mt-5 ">
+            <Link to={`/pUsers/editParty/${el._id}`}>
+              <FaEdit className="text-blue-500" />
+            </Link>
+
+            <MdDelete
+              onClick={() => {
+                deleteHandler(el._id);
+              }}
+              className="text-red-500"
+            />
+          </div>
+
+          <hr className="mt-6" />
+        </div>
+      </>
+    );
+  };
+
+  return (
+    <div className="flex relative h-screen ">
+      <div>
+        <Sidebar TAB={"invoice"} showBar={showSidebar} />
+      </div>
+
+      <div className="flex-1 bg-slate-50 overflow-y-scroll ">
+        <div className="sticky top-0 z-20">
+          <div className="bg-[#012a4a] shadow-lg px-4 py-3 pb-3 flex  items-center gap-2   ">
+            <Link to={"/pUsers/dashboard"}>
+              <IoIosArrowRoundBack className="text-3xl text-white cursor-pointer md:hidden" />
+            </Link>
+            <p className="text-white text-lg   font-bold ">Invoices</p>
+          </div>
+
+          {/* invoiec date */}
+          <div className=" p-4  bg-white drop-shadow-lg">
+            <div className="flex justify-between  items-center">
+              {/* <div className=" flex flex-col gap-1 justify-center">
+          <p className="text-md font-semibold text-violet-400">
+            Search Parties
+          </p>
+        </div>
+        <div className="flex items-center hover_scale cursor-pointer">
+          <p className="text-pink-500 m-2 cursor-pointer  ">Cancel</p>
+          <MdCancel className="text-pink-500" />
+        </div> */}
+            </div>
+            <div className=" md:w-1/2 ">
+              {/* search bar */}
+              <div className="relative  ">
+                <div className="absolute inset-y-0 start-0 flex items-center  pointer-events-none ">
+                  <svg
+                    className="w-4 h-4 text-gray-500 "
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                    />
+                  </svg>
+                </div>
+                <div class="relative">
+                  <input
+                    onChange={(e) => setSearch(e.target.value)}
+                    value={search}
+                    type="search"
+                    id="default-search"
+                    class="block w-full p-2  text-sm text-gray-900 border  rounded-lg border-gray-300  bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+                    placeholder="Search party by name..."
+                    required
+                  />
+                  <button
+                    type="submit"
+                    class="text-white absolute end-[10px] top-1/2 transform -translate-y-1/2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-md px-2 py-1"
+                  >
+                    <IoIosSearch />
+                  </button>
+                </div>
+              </div>
+
+              {/* search bar */}
+            </div>
+          </div>
+        </div>
+
+        {/* adding party */}
+
+        {loader ? (
+          <div className="flex justify-center items-center h-screen">
+            <HashLoader color="#363ad6" />
+          </div>
+        ) : invoice.length > 0 ? (
+          <div
+            style={{
+              scrollbarWidth: "thin",
+              scrollbarColor: "transparent transparent",
+            }}
+          >
+            <List
+              className=""
+              height={600} // Specify the height of your list
+              itemCount={filteredInvoice.length} // Specify the total number of items
+              itemSize={120} // Specify the height of each item
+              width="100%" // Specify the width of your list
+            >
+              {Row}
+            </List>
+          </div>
+        ) : (
+          <div className="font-bold flex justify-center items-center mt-12 text-gray-500">
+            No Invoices found !!!
+          </div>
+        )}
+
+        {/* <Link to={"/pUsers/addParty"} className="flex justify-center">
+        <div className=" px-4  absolute bottom-2 text-white bg-violet-700 rounded-3xl p-2 flex items-center justify-center gap-2 hover_scale cursor-pointer ">
+          <IoIosAddCircle className="text-2xl" />
+          <p>Create New Party</p>
+        </div>
+      </Link> */}
+      </div>
+    </div>
+  );
+}
+
+export default InvoiceList;
