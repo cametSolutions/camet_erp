61317b64897e4d4c6be82879fb6d99564a2669c6
MidhunMohan210 <midhunmohan3210@gmail.com>
2024-04-01 12:30:59 +0530

    test git ingnore

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    // baseURL:'http://localhost:7000',
-    baseURL:"https://www.erp.camet.in/"
+    baseURL:'http://localhost:7000',
+    // baseURL:"https://www.erp.camet.in/"
 
 });
