4f4a32e9f8e2f96ef767c01d3cc89a1e038ac30a
MidhunMohan210 <midhunmohan3210@gmail.com>
2024-04-16 16:55:28 +0530

    produts to save from tally function added

--- backend/routes/tallyRoute.js
+++ backend/routes/tallyRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { saveDataFromTally,giveTransaction,addBankData } from "../controllers/tallyController.js";
+import { saveDataFromTally,giveTransaction,addBankData ,saveProductsFromTally} from "../controllers/tallyController.js";
 
 const router =express.Router();
 
 router.post('/master/os',saveDataFromTally)
 router.post('/master/bank',addBankData)
+router.post('/master/item',saveProductsFromTally)
 router.get('/getTransactions/:cmp_id/:SNo',giveTransaction)
 
 
