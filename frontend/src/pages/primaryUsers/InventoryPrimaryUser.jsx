fa8c4945a2f87fde05aac919212513d2bf39ba37
MidhunMohan210 <midhunmohan3210@gmail.com>
2024-04-22 10:41:38 +0530

    added secondary inventory

--- frontend/src/pages/primaryUsers/InventoryPrimaryUser.jsx
+++ frontend/src/pages/primaryUsers/InventoryPrimaryUser.jsx
@@ -19,7 +19,6 @@ import { IoIosArrowRoundBack } from "react-icons/io";
 
 import { useDispatch } from "react-redux";
 
-import { IoIosArrowRoundBack } from "react-icons/io";
 
 
 function InventoryPrimaryUser() {
