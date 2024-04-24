f5ee378c91c9b4a27426d97c55d94643df3bf93e
MidhunMohan210 <midhunmohan3210@gmail.com>
2024-04-24 15:57:24 +0530

    api changed

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { FaWindows } from 'react-icons/fa';
 
 const api = axios.create({
-  baseURL: 'http://localhost:7000',
-  // baseURL:"https://www.erp.camet.in/"
+  // baseURL: 'http://localhost:7000',
+  baseURL:"https://www.erp.camet.in/"
 });
 
 
