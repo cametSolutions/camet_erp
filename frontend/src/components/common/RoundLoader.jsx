cd0dce764a7823d47338540b4e1cad506bae8a9f
MidhunMohan210 <midhunmohan3210@gmail.com>
2024-05-31 16:19:06 +0530

    batch updations added to main

---
+++ frontend/src/components/common/RoundLoader.jsx
@@ -0,0 +1,9 @@
+function RoundLoader() {
+  return (
+    <div className="h-screen flex justify-center items-center absolute">
+      <div className="border-gray-300 h-10 w-10 animate-spin rounded-full border-4 border-t-blue-600" />
+    </div>
+  );
+}
+
+export default RoundLoader;
