9dac4df8da05a8c965393398138fa553195390fd
MidhunMohan210 <midhunmohan3210@gmail.com>
2024-08-09 14:15:37 +0530

    modified pos print

--- frontend/src/components/common/SalesThreeInchPdf.jsx
+++ frontend/src/components/common/SalesThreeInchPdf.jsx
@@ -90,31 +90,30 @@ function SalesThreeInchPdf({
   return (
     <div
       ref={contentToPrint}
-      style={{ width: "80mm",height:"auto" }}
+      style={{ width: "80mm",height:"auto", }}
 
-      className="rounded-lg  px-3   flex justify-center "
+      className="  rounded-lg   flex justify-center pr-1 "
     >
-      <div className=" print-container px-3 max-w-3xl mx-auto  md:block w-[18rem] ">
+      <div className=" print-container  max-w-3xl mx-auto  md:block w-full ">
         <div className="flex justify-center ">
-          <div className="font-bold text-md  mb-2 mt-6">INVOICE</div>
+          <div className="font-bold text-md  mt-6">INVOICE</div>
         </div>
         <div>
-          <div className="bg-gray-500 h-1 w-full mt-1"></div>
-          <div className="flex items-center justify-between  bg-gray-300 px-3 py-1  font-bold">
-            <div className="text-[9px] ">Invoice #:{data?.salesNumber} </div>
-            <div className="text-[9px]">Date:{new Date().toDateString()} </div>
+          <div className="flex items-center justify-between flex-col leading-4   font-bold">
+            <div className="text-[13px]  tracking-wide ">Invoice #:{data?.salesNumber} </div>
+            <div className="text-[13px] tracking-wide">Date:{new Date().toDateString()} </div>
           </div>
         </div>
 
         <div className="flex justify-center">
-          <div className="w-4/5 flex flex-col mt-1 ml-2">
-            <div className=" flex justify-center">
-              <p className="text-black font-bold text-[13px] pb-1">
+          <div className=" flex flex-col  items-center">
+            <div className=" flex justify-center ">
+              <p className="text-black font-extrabold text-[15px] pb-1 text-center">
                 {org?.name}
               </p>
             </div>
-            <div className=" flex flex-col items-center ">
-              <div className="text-black  text-[11px] font-semibold text-center">
+            <div className=" flex flex-col items-center leading-4 ">
+              <div className="text-black  text-[13px] font-semibold text-center">
                 {[
                   org?.flat,
                   org?.landmark,
@@ -126,16 +125,16 @@ function SalesThreeInchPdf({
                   .filter(Boolean) // Remove any falsy values (e.g., undefined or null)
                   .join(", ")}
               </div>
-              <div className="text-black font-semibold   text-[11px] ">
+              <div className="text-black font-semibold   text-[13px] ">
                 {org?.email}
               </div>
-              <div className="text-black font-semibold  text-[11px] ">
+              <div className="text-black font-semibold  text-[13px] ">
                 {org?.website}
               </div>
-              <div className="text-black font-semibold  text-[11px]">
+              <div className="text-black font-semibold  text-[13px]">
                 Gst No: {org?.gstNum}
               </div>
-              <div className="text-black font-semibold   text-[11px]">
+              <div className="text-black font-semibold   text-[13px]">
                 Pan No: {org?.pan}
               </div>
             </div>
@@ -143,11 +142,11 @@ function SalesThreeInchPdf({
         </div>
         {/* </div> */}
 
-        <div className="">
-          <p className="text-black   text-[9px] font-bold mt-6">
+        <div className="leading-4">
+          <p className="text-black   text-[13px] font-bold mt-6 tracking-wider">
             Name: {data?.party?.partyName}
           </p>
-          <p className="text-black text-[9px] font-bold">
+          <p className="text-black text-[13px] font-semibold">
             {[address?.billToAddress,]
               .filter((item) => item != null && item !== "" && item !== "null")
               .join(", ") || "Address not available"}
@@ -155,14 +154,14 @@ function SalesThreeInchPdf({
         </div>
 
         {/* <hr className="border-t-2 border-black mb-0.5" /> */}
-        <table className="w-full text-left  bg-slate-200  mt-1 ">
-          <thead className="border-b border-t-2 border-black text-[10px] text-right">
+        <table className="w-full text-left    mt-2 tracking-wider ">
+          <thead className="border-b border-t-2 border-black text-[10px] text-right ">
             <tr>
-              <th className="text-black font-bold uppercase py-2 px-1 text-left">
+              <th className="text-black font-bold uppercase  px-1 text-left">
                 Items
               </th>
-              <th className="text-black font-bold uppercase p-2">Qty</th>
-              <th className="text-black font-bold uppercase p-2">Rate</th>
+              <th className="text-black font-bold uppercase text-center p-2">Qty</th>
+              <th className="text-black font-bold uppercase text-center p-2">Rate</th>
               {/* <th className="text-black font-bold uppercase p-2">Disc</th> */}
               {/* <th className="text-black font-bold uppercase p-2">Tax</th> */}
               <th className="text-black font-bold uppercase p-2 pr-0">
@@ -171,7 +170,7 @@ function SalesThreeInchPdf({
             </tr>
           </thead>
 
-          <tbody>
+          <tbody >
             {data?.items?.length > 0 &&
               data?.items.map((el, index) => {
                 const total = el?.total || 0;
@@ -183,28 +182,33 @@ function SalesThreeInchPdf({
                 return (
                   <tr
                     key={index}
-                    className="border-b border-t-1 text-[9px] bg-white"
+                    className="border-b border-t-1 text-[12px] bg-white  text-center  "
                   >
-                    <td className="py-2 text-black  font-bold  pr-2">
+                    <td className="py-1 text-black  font-bold  pr-2 flex ">
                       {el.product_name} <br />
-                      <p className="text-black mt-1">
-                        HSN: {el?.hsn_code} ({el.igst}%)
+                      <p className="text-black ">
+                      ({el.igst}%)
                       </p>
                     </td>
-                    <td className="py-2 text-black  font-bold text-right pr-2">
-                      {el?.count} {el?.unit}
+                    <td className="py-1 text-black  font-bold text-right pr-2">
+                      
+                      {el?.count}
+                      <p className="text-[10px] font-semibold">
+
+                       {el?.unit}
+                      </p>
                     </td>
-                    <td className="py-2 text-black font-bold  text-right pr-2 text-nowrap">
-                      ₹ {rate || 0}
+                    <td className="py-1 text-black font-bold  text-right pl-2 pr-1 text-nowrap">
+                      {rate || 0}
                     </td>
 
-                    <td className="py-4 text-black  font-bold text-right">
-                      ₹ {el?.total}
+                    <td className="py-1 text-black  font-bold text-right">
+                      {el?.total}
                     </td>
                   </tr>
                 );
               })}
-            <tr className=" border-y-2 border-t-2 border-gray-500  font-bold  text-[9px] bg-white">
+            <tr className=" border-y-2 border-t-2 border-gray-500  font-bold  text-[13px] bg-white">
               <td className="py-1 text-black ">Total</td>
               <td className=" col-span-2 py-1 text-black text-center">
                 {" "}
@@ -214,27 +218,16 @@ function SalesThreeInchPdf({
                 )}
               </td>
               <td className="py-1 text-black "></td>
-              <td className="py-1 text-black text-right "> ₹ {subTotal}</td>
+              <td className="py-1 text-black text-right "> {subTotal}</td>
             </tr>
           </tbody>
         </table>
 
         <div className="flex justify-end">
-          <div className="mt-3 w-1/2 ">
+          {/* <div className="mt-3 w-1/2 ">
             {bank && Object.keys(bank).length > 0 ? (
               <>
-                {/* <div className="text-black font-semibold text-[10px] leading-5">
-                Bank Name: {bank?.bank_name}
-              </div>
-              <div className="text-black font-semibold text-[10px] leading-5">
-                IFSC Code: {bank?.ifsc}
-              </div>
-              <div className="text-black font-semibold text-[10px] leading-5">
-                Account Number: {bank?.ac_no}
-              </div>
-              <div className="text-black font-semibold text-[10px] leading-5">
-                Branch: {bank?.branch}
-              </div> */}
+          
                 <div
                   style={{
                     height: "auto",
@@ -259,21 +252,19 @@ function SalesThreeInchPdf({
             ) : (
               <div className="text-black font-semibold text-[10px] leading-5"></div>
             )}
-          </div>
+          </div> */}
 
-          <div className="w-1/2">
-            <div className=" mt-3  ">
+            <div className=" mt-1  ">
               <div className="  flex flex-col items-end ">
-                <div className="flex flex-col items-end text-[9px] text-black font-bold gap-1">
+                <div className="flex flex-col items-end text-[13px] text-black font-bold gap-1">
                   <p className={calculateTotalTax() > 0 ? "" : "hidden"}>
                     CGST : {(calculateTotalTax() / 2).toFixed(2)}
                   </p>
                   <p className={calculateTotalTax() > 0 ? "" : "hidden"}>
                     SGST : {(calculateTotalTax() / 2).toFixed(2)}
                   </p>
-                  {/* <p className={calculateTotalTax() > 0 ? "" : "hidden"}>
-                IGST : {calculateTotalTax()}
-              </p> */}
+             
+        
                   <p className={calculateCess() > 0 ? "" : "hidden"}>
                     CESS : {calculateCess()}
                   </p>
@@ -285,12 +276,12 @@ function SalesThreeInchPdf({
                   </p>
                 </div>
 
-                <div className="flex items-center mt-2 mb-1">
-                  <div className="text-black mr-2 font-bold text-[9px] ">
+                <div className="flex items-center mt-1 mb-1">
+                  <div className="text-black mr-2 font-bold text-[12px] ">
                     Add on charges:
                   </div>
-                  <div className="text-black font-bold text-[9px]">
-                    ₹ {additinalCharge}
+                  <div className="text-black font-bold text-[12px]">
+                     {additinalCharge}
                   </div>
                 </div>
               </div>
@@ -298,7 +289,7 @@ function SalesThreeInchPdf({
                 <>
                   <div
                     key={index}
-                    className="text-black  text-right font-semibold text-[9px] "
+                    className="text-black  text-right font-semibold text-[12px] "
                   >
                     <span>({el?.action === "add" ? "+" : "-"})</span>{" "}
                     {el?.option}: ₹ {el?.finalValue}
@@ -310,14 +301,13 @@ function SalesThreeInchPdf({
                   )}
                 </>
               ))}
-            </div>
 
             <div className="flex justify-end  border-black  ">
               <div className="w-3/4"></div>
 
-              <div className=" w-2/4 text-black  font-extrabold text-[11px] flex justify-end   ">
-                <p className="text-nowrap border-y-2 py-2">NET AMOUNT : </p>
-                <div className="text-black  font-bold text-[11px] text-nowrap  border-y-2 py-2    ">
+              <div className="  text-black  font-extrabold text-[13px] flex justify-end   ">
+                <p className="text-nowrap border-y-2 py-1">NET AMOUNT : </p>
+                <div className="text-black  font-bold text-[13px] text-nowrap  border-y-2 py-1    ">
                   ₹ {data?.finalAmount}
                 </div>
               </div>
@@ -325,9 +315,9 @@ function SalesThreeInchPdf({
             <div className="flex  justify-end border-black pb-3 w-full ">
               <div className="w-2/4"></div>
 
-              <div className="text-black font-bold text-[10px] flex flex-col justify-end text-right mt-3">
+              <div className="text-black font-bold text-[12px] flex flex-col justify-end text-right mt-1">
                 <p className="text-nowrap">Total Amount(in words)</p>
-                <div className="text-black full font-bold text-[7.5px] text-nowrap uppercase mt-1   ">
+                <div className="text-black full font-bold text-[12px] text-nowrap uppercase mt-1   ">
                   <p className="whitespace-normal">₹ {inWords}</p>
                 </div>
               </div>
@@ -359,6 +349,36 @@ function SalesThreeInchPdf({
         </div> */}
           </div>
         </div>
+
+            {/* <div className="mt-3  flex justify-center ">
+            {bank && Object.keys(bank).length > 0 ? (
+              <>
+          
+                <div
+                  style={{
+                    height: "auto",
+                    margin: "0 ",
+                    marginTop: "10px",
+                    maxWidth: 90,
+                    width: "100%",
+                  }}
+                >
+                  <QRCode
+                    size={300}
+                    style={{
+                      height: "auto",
+                      maxWidth: "100%",
+                      width: "100%",
+                    }}
+                    value={`upi://pay?pa=${bank?.upi_id}&am=${data?.finalAmount}`}
+                    viewBox={`0 0 256 256`}
+                  />
+                </div>
+              </>
+            ) : (
+              <div className="text-black font-semibold text-[10px] leading-5"></div>
+            )}
+          </div> */}
 
         {/* {org && org.configurations?.length > 0 && (
       <div className="border-gray-300 mb-5 mt-4">
