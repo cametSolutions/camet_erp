c82df5cff99b216d9effdbfa5ce9ae13110d27fe
MidhunMohan210 <midhunmohan3210@gmail.com>
2024-08-11 16:37:55 +0530

    api change

--- frontend/src/pages/secUsers/ThreeInchSalesSec.jsx
+++ frontend/src/pages/secUsers/ThreeInchSalesSec.jsx
@@ -115,7 +115,7 @@ function ThreeInchSalesSec() {
 
   return (
     <div >
-      <div className="  nonPrintable-content bg-[#012a4a]   sticky top-0 p-3 px-5 text-white text-lg font-bold flex items-center gap-3  shadow-lg justify-between">
+      <div className=" nonPrintable-content bg-[#012a4a]   sticky top-0 p-3 px-5 text-white text-lg font-bold flex items-center gap-3  shadow-lg justify-between">
         <div className="flex gap-2 ">
           <Link to={`/sUsers/salesDetails/${id}`}>
             <IoIosArrowRoundBack className="text-3xl" />
@@ -142,20 +142,8 @@ function ThreeInchSalesSec() {
           additinalCharge={additinalCharge}
           inWords={inWords}
         />
-      </div>
-{/* 
-      <div>
-          <MdPrint
-            onClick={() => {
-              handlePrint(null, () => contentToPrint.current);
-            }}
-            className="text-xl cursor-pointer "
-          />
-        </div> */}
-
-      
-
-      {/* <PrintButton salesId={id}/> */}
+      </div> 
+    
     </div>
   );
 }
