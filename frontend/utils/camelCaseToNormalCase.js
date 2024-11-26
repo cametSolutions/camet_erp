de573e66c9ba6f1899ed50dd9ce74a431e87cd11
MidhunMohan210 <midhumnohan3210@gmail.com>
2024-11-26 11:29:07 +0530

    replce blank issue solved

--- frontend/utils/camelCaseToNormalCase.js
+++ frontend/utils/camelCaseToNormalCase.js
@@ -1,6 +1,6 @@
 export const camelToNormalCase = (str) => {
   // Replace capital letters with space followed by the letter in lowercase
-  const result = str.replace(/([A-Z])/g, " $1");
+  const result = str?.replace(/([A-Z])/g, " $1");
   // Capitalize the first letter and trim any leading spaces
-  return result.charAt(0).toUpperCase() + result.slice(1).toLowerCase();
+  return result?.charAt(0)?.toUpperCase() + result?.slice(1)?.toLowerCase();
 };
