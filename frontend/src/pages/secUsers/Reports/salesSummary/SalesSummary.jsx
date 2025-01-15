999adee903ba044929f5816d3340af7e4e534343
ABHIDAS-S <abhidasabhi1234@gmail.com>
2025-01-15 17:43:03 +0530

    category

--- frontend/src/pages/secUsers/Reports/salesSummary/SalesSummary.jsx
+++ frontend/src/pages/secUsers/Reports/salesSummary/SalesSummary.jsx
@@ -53,26 +53,26 @@ function SalesSummary() {
   }, [salesummaryData])
   useEffect(() => {
     if (summary && summary.length > 0) {
+      setSelectedIndex(null)
       handleLedger(selectedOption)
     }
   }, [summary])
+  console.log(selectedOption)
   const handleLedger = (option) => {
+    setSelectedOption(option)
     let check = []
     let arr = []
     console.log(option)
     if (option === "Ledger") {
       const partybase = summary.map((item) => {
+        console.log(item)
         console.log(check)
         let existingParty = check.find((data) => {
           console.log(data.partyId)
           console.log(item.party._id)
           return data.partyId === item.party?._id
         })
-        // const object = {
-        //   partyName: item?.party?.partyName,
-        //   partyId: item?.party?._id,
-        //   sale: []
-        // }
+
         console.log(existingParty)
         if (existingParty) {
           let c = 0
@@ -168,6 +168,7 @@ function SalesSummary() {
           }
           let a = 0
           const sale = item.items.map((it) => {
+            console.log(it)
             if (it.hasGodownOrBatch) {
               if (
                 it.GodownList.every((item) => item.godown_id) && // Check all have godown_id
@@ -176,12 +177,17 @@ function SalesSummary() {
                 const godown = it.GodownList.reduce((acc, items) => {
                   if (items.added) {
                     if (Object.keys(acc).length > 0) {
+                      console.log(Object.keys(acc).length > 0)
+                      console.log("j")
                       // Update the existing object's values
                       acc.quantity += items?.count || 0
                       acc.discount += items?.discount || 0
                       acc.netAmount += items?.individualTotal || 0
                       acc.taxAmount += items?.igstAmt || 0
+                      console.log(items?.individualTotal)
+                      console.log(a)
                       a += items?.individualTotal
+                      console.log(a)
                     } else {
                       // Populate the object for the first time
                       Object.assign(acc, {
@@ -197,13 +203,21 @@ function SalesSummary() {
                         netAmount: items?.individualTotal || 0
                       })
 
+                      console.log(a)
                       a += items?.individualTotal
+                      console.log(a)
                     }
                     return acc
                   }
                 }, {})
-                object.saleAmount = a
+                console.log(a)
+                console.log(object.saleAmount)
+
+                object.saleAmount += a
+                console.log(a)
+                console.log(object.saleAmount)
                 object.sale.push(godown)
+                console.log(object)
               } else {
                 const godown = it.GodownList.map((items) => {
                   if (items.added) {
@@ -220,10 +234,14 @@ function SalesSummary() {
                       taxAmount: it?.igstAmt,
                       netAmount: items?.individualTotal
                     }
+                    console.log(object)
+                    console.log(items?.individualTotal, items?.count)
                     object.saleAmount += items?.individualTotal
+                    console.log(object)
                     object.sale.push(a)
                   }
                 })
+                console.log(object)
               }
             } else {
               const godown = it.GodownList.map((items) => {
@@ -240,9 +258,14 @@ function SalesSummary() {
                   taxAmount: it?.igstAmt,
                   netAmount: items?.individualTotal
                 }
+                console.log(object.saleAmount)
+                console.log(items?.individualTotal)
                 object.saleAmount += items?.individualTotal
+                console.log(object.saleAmount)
                 object.sale.push(a)
+                console.log(object.saleAmount)
               })
+              console.log(object)
             }
           })
           console.log(object)
@@ -257,6 +280,204 @@ function SalesSummary() {
       console.log("checkk", check)
     } else if (option === "Stock Group") {
       console.log("hii")
+      const partybase = summary.map((item) => {
+        const z = item.items.map((h) => {
+          if (h?.brand?.name) {
+            console.log(item)
+            console.log(h.brand.name)
+            console.log(h)
+            let existingParty = check.find((data) => {
+              return data.groupId === h.brand?._id
+            })
+            console.log(existingParty)
+            if (existingParty) {
+              console.log(existingParty)
+              let c = 0
+              console.log(existingParty)
+              const sale = item.items.map((it) => {
+                if (it.hasGodownOrBatch) {
+                  if (
+                    it.GodownList.every((item) => item.godown_id) && // Check all have godown_id
+                    it.GodownList.every((item) => !item.hasOwnProperty("batch")) // Ensure no item has batch
+                  ) {
+                    const godown = it.GodownList.reduce((acc, items) => {
+                      const existing = acc.find(
+                        (entry) => entry.godown_id === items.godown_id
+                      )
+
+                      if (existing && items.added) {
+                        console.log("inddddd")
+                        // Update totals for the existing entry
+                        existing.quantity += items?.count || 0
+                        existing.discount += items?.discount || 0
+                        existing.netAmount += items?.individualTotal || 0
+                        existing.taxAmount += items?.igstAmt || 0
+                        c += items?.individualTotal
+                      } else if (!existing && items.added) {
+                        // Add a new entry for this godown_id
+                        acc.push({
+                          godown_id: items?.godown_id,
+                          billnumber: item?.salesNumber,
+                          billDate: item?.date,
+                          itemName: it?.product_name,
+                          groupName: it?.brand?.name,
+                          quantity: items?.count || 0,
+                          rate: items?.selectedPriceRate || 0,
+                          discount: items?.discount || 0,
+                          taxPercentage: it?.igst || 0,
+                          taxAmount: items?.igstAmt || 0,
+                          netAmount: items?.individualTotal || 0
+                        })
+                        c += items?.individualTotal
+                      }
+                      return acc
+                    }, [])
+                    existingParty.saleAmount = c
+                    existingParty.sale.push(godown)
+                  } else {
+                    const godown = it.GodownList.map((items) => {
+                      if (items.added) {
+                        const a = {
+                          billnumber: item?.salesNumber,
+                          billDate: item?.date,
+                          itemName: it?.product_name,
+                          batch: items?.batch,
+                          groupName: it?.brand?.name,
+                          quantity: items?.count,
+                          rate: items?.selectedPriceRate,
+                          discount: items?.discount,
+                          taxPercentage: it?.igst,
+                          taxAmount: it?.igstAmt,
+                          netAmount: items?.individualTotal
+                        }
+                        existingParty.saleAmount += items?.individualTotal
+                        existingParty.sale.push(a)
+                      }
+                    })
+                  }
+                } else {
+                  console.log("hh")
+                  const godown = it.GodownList.map((items) => {
+                    const a = {
+                      billnumber: item?.salesNumber,
+                      billDate: item?.date,
+                      itemName: it?.product_name,
+
+                      groupName: it?.brand?.name,
+                      quantity: it?.count,
+                      rate: items?.selectedPriceRate,
+                      discount: it?.discount,
+                      taxPercentage: it?.igst,
+                      taxAmount: it?.igstAmt,
+                      netAmount: items?.individualTotal
+                    }
+                    existingParty.saleAmount += items?.individualTotal
+                    existingParty.sale.push(a)
+                  })
+                }
+              })
+            } else {
+              const object = {
+                groupName: h?.brand?.name,
+                groupId: h?.brand?._id,
+                sale: [],
+                saleAmount: 0
+              }
+              console.log(object)
+              let a = 0
+
+              if (h.hasGodownOrBatch) {
+                console.log("hii")
+                if (
+                  it.GodownList.every((item) => item.godown_id) && // Check all have godown_id
+                  it.GodownList.every((item) => !item.hasOwnProperty("batch")) // Ensure no item has batch
+                ) {
+                  console.log("gggg")
+                  const godown = it.GodownList.reduce((acc, items) => {
+                    if (items.added) {
+                      if (Object.keys(acc).length > 0) {
+                        // Update the existing object's values
+                        acc.quantity += items?.count || 0
+                        acc.discount += items?.discount || 0
+                        acc.netAmount += items?.individualTotal || 0
+                        acc.taxAmount += items?.igstAmt || 0
+                        a += items?.individualTotal
+                      } else {
+                        // Populate the object for the first time
+                        Object.assign(acc, {
+                          billnumber: item?.salesNumber,
+                          billDate: item?.date,
+                          itemName: it?.product_name,
+                          groupName: it?.brand?.name,
+                          quantity: items?.count || 0,
+                          rate: items?.selectedPriceRate || 0,
+                          discount: items?.discount || 0,
+                          taxPercentage: it?.igst || 0,
+                          taxAmount: items?.igstAmt || 0,
+                          netAmount: items?.individualTotal || 0
+                        })
+
+                        a += items?.individualTotal
+                      }
+                      return acc
+                    }
+                  }, {})
+                  object.saleAmount = a
+                  object.sale.push(godown)
+                } else {
+                  console.log("hi")
+                  const godown = it.GodownList.map((items) => {
+                    if (items.added) {
+                      const a = {
+                        billnumber: item?.salesNumber,
+                        billDate: item?.date,
+                        itemName: it?.product_name,
+                        batch: items?.batch,
+                        groupName: it?.brand?.name,
+                        quantity: items?.count,
+                        rate: items?.selectedPriceRate,
+                        discount: items?.discount,
+                        taxPercentage: it?.igst,
+                        taxAmount: it?.igstAmt,
+                        netAmount: items?.individualTotal
+                      }
+                      object.saleAmount += items?.individualTotal
+                      object.sale.push(a)
+                    }
+                  })
+                }
+              } else {
+                console.log("uu")
+                console.log(h)
+                console.log(item)
+                const godown = h.GodownList.map((items) => {
+                  console.log(items)
+                  const a = {
+                    billnumber: item?.salesNumber,
+                    billDate: item?.date,
+                    itemName: h?.product_name,
+
+                    partyName: item?.party?.partyName,
+                    quantity: h?.count,
+                    rate: items?.selectedPriceRate,
+                    discount: h?.discount,
+                    taxPercentage: h?.igst,
+                    taxAmount: h?.igstAmt,
+                    netAmount: items?.individualTotal
+                  }
+                  console.log(a)
+                  object.saleAmount += items?.individualTotal
+                  object.sale.push(a)
+                })
+              }
+
+              console.log(object)
+              check.push(object)
+            }
+          }
+        })
+      })
+      setSummaryReport(check)
     }
   }
 
@@ -264,7 +485,7 @@ function SalesSummary() {
     setUserAndCompanyData(data)
   }
   console.log(summaryReport)
-
+  console.log(selectedOption)
   return (
     <div>
       <div className="sticky top-0 z-50">
@@ -292,8 +513,8 @@ function SalesSummary() {
               className="w-full sm:max-w-sm md:max-w-xs bg-[#219ebc] text-white font-semibold py-2 px-3 rounded-lg shadow-lg hover:shadow-xl cursor-pointer appearance-none focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
               value={selectedOption}
               onChange={(e) => {
+                console.log(e.target.value)
                 handleLedger(e.target.value)
-                setSelectedOption(e.target.value)
               }}
             >
               <option value="Ledger">Ledger</option>
@@ -333,7 +554,7 @@ function SalesSummary() {
                 View Details
               </button>
             </div>
-            <div className="mb-4">
+            <div className="mb-2">
               {summaryReport &&
                 summaryReport.length > 0 &&
                 selectedIndex === null &&
@@ -343,7 +564,7 @@ function SalesSummary() {
                     className="flex justify-between items-center mb-4 p-4 bg-white rounded-lg shadow-md transform transition duration-300 hover:shadow-xl hover:scale-55 hover:-translate-y-1"
                   >
                     <span className="text-gray-800 font-medium">
-                      {item.partyName}
+                      {item.partyName || item.groupName}
                     </span>
                     <span className="text-gray-600 font-semibold">
                       {item.saleAmount}
@@ -355,10 +576,10 @@ function SalesSummary() {
         )}
 
         {selectedIndex !== null && (
-          <div className="p-4 bg-gray-50 rounded-lg shadow-lg mt-4">
-            <div className="flex justify-between items-center mb-4">
+          <div className="px-2 bg-gray-50 rounded-lg shadow-lg ">
+            <div className="flex justify-between items-center mb-1  ">
               <button
-                className="px-4 py-2 bg-red-600 text-white rounded-lg shadow hover:bg-red-700 transition duration-200 cursor-pointer"
+                className="px-4 py-1 bg-red-600 text-white rounded-lg shadow hover:bg-red-700 transition duration-200 cursor-pointer"
                 onClick={() => setSelectedIndex(null)} // Reset to close the table view
               >
                 Close
@@ -368,13 +589,23 @@ function SalesSummary() {
               <thead>
                 <tr className="border-b bg-gray-200">
                   <th className="p-2 font-semibold text-gray-600">
-                    Party Name
+                    {" "}
+                    {selectedOption === "Ledger"
+                      ? "Party Name"
+                      : selectedOption === "Stock Group"
+                      ? "Group Name"
+                      : selectedOption === "Stock Category"
+                      ? "Category Name"
+                      : selectedOption === "Stock Item"
+                      ? "Item Name"
+                      : ""}
                   </th>
                   <th className="p-2 font-semibold text-gray-600">Bill No</th>
                   <th className="p-2 font-semibold text-gray-600">Bill Date</th>
                   <th className="p-2 font-semibold text-gray-600">
-                    Group Name
+                    {selectedOption === "Ledger" ? "Group Name" : "Party Name"}
                   </th>
+                  <th className="p-2 font-semibold text-gray-600">Batch</th>
                   <th className="p-2 font-semibold text-gray-600">Item Name</th>
                   <th className="p-2 font-semibold text-gray-600">Quantity</th>
                   <th className="p-2 font-semibold text-gray-600">Rate</th>
@@ -402,9 +633,10 @@ function SalesSummary() {
                           className="p-2 text-gray-800 font-bold"
                           rowSpan={party.sale.length} // Merge rows for the same party
                         >
-                          {party.partyName}
+                          {party.partyName || party.groupName}
                         </td>
                       ) : null}
+
                       <td className="p-2 text-gray-800">
                         {saleItem?.billnumber}
                       </td>
@@ -416,7 +648,10 @@ function SalesSummary() {
                             .split("T")[0]
                         }
                       </td>
-                      <td className="p-2 text-gray-800">{saleItem?.brand}</td>
+                      <td className="p-2 text-gray-800">
+                        {saleItem?.groupName || saleItem?.partyName}
+                      </td>
+                      <td className="p-2 text-gray-800">{saleItem?.batch}</td>
                       <td className="p-2 text-gray-800">
                         {saleItem?.itemName}
                       </td>
