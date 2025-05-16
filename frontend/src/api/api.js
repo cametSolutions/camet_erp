cd2bf0aae4b5337a210346003b3c4380e3a0050b
MidhunMohan210 <midhunmohan3210@gmail.com>
2025-05-16 14:48:17 +0530

    added -ve stock in vansale in main

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,10 +2,10 @@ import axios from "axios";
 import Swal from "sweetalert2";
 
 const api = axios.create({
-  baseURL: "http://localhost:7000",
+  // baseURL: "http://localhost:7000",
   // baseURL:"https://www.erp.camet.in/"
   // baseURL:"https://www.erptest.camet.in/"
-  // baseURL:"https://www.app.camet.in/"
+  baseURL:"https://www.app.camet.in/"
 });
 
 api.interceptors.response.use(
