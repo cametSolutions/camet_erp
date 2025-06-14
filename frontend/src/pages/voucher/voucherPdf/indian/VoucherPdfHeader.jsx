796787ec76bec8c840e97eaff6af169dcbb125a9
MidhunMohan210 <midhunmohan3210@gmail.com>
2025-06-14 16:16:59 +0530

    date issue in pdf solved

--- frontend/src/pages/voucher/voucherPdf/indian/VoucherPdfHeader.jsx
+++ frontend/src/pages/voucher/voucherPdf/indian/VoucherPdfHeader.jsx
@@ -68,7 +68,7 @@ function VoucherPdfHeader({
             {getTitleNumber()} : {data?.[getVoucherNumber()]}
           </div>
           <div className="text-xs md:text-sm">
-            Date: {new Date(data?.createdAt).toDateString()}
+            Date: {new Date(data?.date).toDateString()}
           </div>
         </div>
       </div>
