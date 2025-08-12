c40d4e3f3221c4d56a7aebe8df56f9e85194ce72
cametSolutions <cametcloud@gmail.com>
2025-08-12 23:13:28 +0530

    updation on conflit

--- backend/routes/secondaryUserRouters.js
+++ backend/routes/secondaryUserRouters.js
@@ -60,8 +60,10 @@ import { createVoucherSeries, getSeriesByVoucher, deleteVoucherSeriesById, editV
 import {saveAdditionalPax , getAdditionalPax ,updateAdditionalPax , deleteAdditionalPax,saveVisitOfPurpose,getVisitOfPurpose,
     updateVisitOfPurpose,deleteVisitOfPurpose,saveIdProof,getIdProof,updateIdProof , deleteIdProof, saveFoodPlan , getFoodPlan
     ,updateFoodPlan,deleteFoodPlan,addRoom,getRooms,editRoom ,deleteRoom,getAllRooms,roomBooking,getBookings,deleteBooking,updateBooking,
-fetchAdvanceDetails} from '../controllers/hotelController.js'
-import {addItem,getAllItems,getItems,getCategories,deleteItem,updateItem,generateKot,getKot,updateKotStatus,getRoomDataForRestaurant,updateKotPayment} from '../controllers/restaurantController.js'
+fetchAdvanceDetails,getAllRoomsWithStatusForDate,updateRoomStatus} from '../controllers/hotelController.js'
+import {addItem,getAllItems,getItems,getCategories,deleteItem,updateItem,generateKot,getKot,updateKotStatus,
+    getRoomDataForRestaurant,updateKotPayment,getPaymentType,saveTableNumber,getTables,deleteTable} from '../controllers/restaurantController.js'
+
 
 router.post('/login',login)
 router.post('/sendOtp',sendOtp)
@@ -337,11 +339,25 @@ router.put('/updateRoomBooking/:id',authSecondary,secondaryIsBlocked,updateBooki
 router.get('/getBookingAdvanceData/:id',authSecondary,secondaryIsBlocked,fetchAdvanceDetails)
 router.post('/generateKOT/:cmp_id',authSecondary,secondaryIsBlocked,generateKot)
 
-router.put('/editItem/:cmp_id/:id',authSecondary,updateItem)
+router.post('/editItem/:cmp_id/:id',authSecondary,updateItem)
 router.get('/getItems/:cmp_id',authSecondary,getItems)
 router.delete('/deleteItem/:id',authSecondary,deleteItem)
 router.get('/getKotData/:cmp_id',authSecondary,secondaryIsBlocked,getKot)
 router.put('/updateKotStatus/:cmp_id',authSecondary,secondaryIsBlocked,updateKotStatus)
 router.get('/getRoomBasedOnBooking/:cmp_id',authSecondary,secondaryIsBlocked,getRoomDataForRestaurant)
-router.put("/updateKotPayment/:id",authSecondary,secondaryIsBlocked,updateKotPayment)
+router.put("/updateKotPayment/:cmp_id/:id",authSecondary,secondaryIsBlocked,updateKotPayment)
+router.get('/getAllRoomsWithStatus/:cmp_id',authSecondary,getAllRoomsWithStatusForDate)
+router.post("/updateStatus/:id", authSecondary,updateRoomStatus);
+router.get("/getPaymentType/:cmp_id",authSecondary,secondaryIsBlocked, getPaymentType)
+router.get("/getSeriesByVoucherForSaleAndReceipt/:cmp_id",authSecondary,secondaryIsBlocked)
+router.post("/Table/:cmp_id",authSecondary, saveTableNumber)
+router.put('/updateTable/:id', authSecondary,updateTable);
+router.get('/getTable/:cmp_id',authSecondary, getTables);
+// DELETE /api/sUsers/Table/:id - Delete a specific table
+router.delete('/deleteTable/:id', authSecondary,deleteTable);
+
+// router.get("/getSalePrintData/:cmp_id/:kotId",authSecondary,secondaryIsBlocked,getSalePrintData)
+
+// Route to get detailed booking information for a specific room and date
+
 export default router
