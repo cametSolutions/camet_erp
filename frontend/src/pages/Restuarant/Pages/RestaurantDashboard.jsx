6a51d89fe04a95c9a6912ccb4af727e64e351bcc
ruksanakasim <anziballianze@gmail.com>
2026-02-05 14:35:31 +0530

    branch change

--- frontend/src/pages/Restuarant/Pages/RestaurantDashboard.jsx
+++ frontend/src/pages/Restuarant/Pages/RestaurantDashboard.jsx
@@ -936,13 +936,16 @@ const RestaurantPOS = () => {
       })
       if (response.data?.success) {
         handleKotPrint(response.data?.data)
-        if (orderType === "dine-in") {
-  await api.put(
-    `/api/sUsers/updateTableStatus/${cmp_id}/${selectedTableNumber}`,  // ✅ Add tableNumber to URL
-    { status: "occupied" },  // Only status in body
-    { withCredentials: true }
-  );
-}
+         if (orderType === "dine-in") {
+          await api.put(
+            '/api/sUsers/updateTableStatus/${cmp_id}',
+            {
+              tableNumber: selectedTableNumber,
+              status: "occupied"
+            },
+            { withCredentials: true }
+          )
+        }
 
         queryClient.invalidateQueries({
           queryKey: ["todaysTransaction", cmp_id, isAdmin]
