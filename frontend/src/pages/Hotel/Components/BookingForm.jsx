c801861e679b0c9cf42805f5c7efc870e8ebb97d
cametSolutions <cametcloud@gmail.com>
2026-04-30 07:15:09 +0530

    updation on live1

--- frontend/src/pages/Hotel/Components/BookingForm.jsx
+++ frontend/src/pages/Hotel/Components/BookingForm.jsx
@@ -1818,7 +1818,7 @@ const otherChargeWithOutTax = charge.reduce((acc, item) => {
                           type="number"
                           name="advanceAmount"
                           value={formData?.advanceAmount}
-                          onChange={}
+                          onChange={handleAdvanceAmountChange}
                           className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                         />
                         {errorObject?.advanceAmount &&
