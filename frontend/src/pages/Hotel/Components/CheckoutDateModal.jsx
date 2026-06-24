1cdf40f35fce18a9a90e29beb0ba8238e04bae07
ruksanakasim <anziballianze@gmail.com>
2026-06-24 13:33:30 +0530

    time

--- frontend/src/pages/Hotel/Components/CheckoutDateModal.jsx
+++ frontend/src/pages/Hotel/Components/CheckoutDateModal.jsx
@@ -1,10 +1,26 @@
 /* eslint-disable react/prop-types */
 
 
+const formatTimeForInput = (time12h = "") => {
+  if (!time12h) return "";
+
+  const [time, modifier] = time12h.split(" ");
+  if (!time || !modifier) return "";
+
+  let [hours, minutes] = time.split(":");
+  hours = parseInt(hours, 10);
+
+  if (modifier.toUpperCase() === "PM" && hours !== 12) hours += 12;
+  if (modifier.toUpperCase() === "AM" && hours === 12) hours = 0;
+
+  return `${String(hours).padStart(2, "0")}:${minutes}`;
+};
+
 export default function CheckoutDateModal({
   checkouts,
   onDateChange,
   onDaysChange,
+  onTimeChange,
 }) {
 console.log(checkouts);
   return (
@@ -14,6 +30,7 @@ console.log(checkouts);
           <tr>
             <th className="p-2 text-left">VoucherNo & Arrival Date</th>
             <th className="p-2 text-left">Checkout Date</th>
+            <th className="p-2 text-left">Checkout Time</th>
             <th className="p-2 text-left">Days</th>
           </tr>
         </thead>
@@ -39,12 +56,14 @@ console.log(checkouts);
                   className="border rounded px-2 py-1 w-full"
                 />
               </td>
-               {/* <td className="p-2 min-w-[180px]">
-                <TimeSelector
-                  initialTime={c.checkOutTime}
-                  onTimeChange={(time) => onTimeChange(c._id, time)}
-                />
-              </td> */}
+               <td className="p-2">
+               <input
+  type="time"
+  value={formatTimeForInput(c.checkOutTime || "")}
+  onChange={(e) => onTimeChange(c._id, e.target.value)}
+  className="border rounded px-2 py-1 w-full"
+/>
+              </td>
               <td className="p-2">
                 <input
                   type="number"
